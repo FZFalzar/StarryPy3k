c377a36dcf876ec42ebbf55479b664e36e23b146
FZFalzar <fzfalzarlw@gmail.com>
2014-03-01 22:54:24 +0800

    Changed load_config to be part of the main plugin thread so it should always load before values are initiated, instead of being async-loaded and risk a desynchronisation. Modified imports to use the latest references

--- plugins/serverads.py
+++ plugins/serverads.py
@@ -1,6 +1,7 @@
 import asyncio
 import random
-from base_plugin import SimpleCommandPlugin, command
+from base_plugin import SimpleCommandPlugin
+from utilities import Command
 from plugins.player_manager import Owner
 
 __author__ = 'FZFalzar'
@@ -12,7 +13,7 @@ class ServerAds(SimpleCommandPlugin):
     #activate is main entry point for plugin
     def activate(self):
         super().activate()
-        asyncio.Task(self.load_config())
+        self.load_config()
         self.prevMsgIdx = 0
         self.rNum = 0
         self.broadcast = asyncio.Task(self.broadcast_thread())
@@ -34,7 +35,7 @@ class ServerAds(SimpleCommandPlugin):
                 print("[%s] %s %s" % (self.name, self.serverads_prefix, self.serverads_list[0]))
                 yield from self.factory.broadcast("%s ^#00FF00;%s" % (self.serverads_prefix, self.serverads_list[0]))
 
-    @command("ads_interval", role=Owner, doc="Sets interval for display of serverads", syntax=("[interval]",))
+    @Command("ads_interval", role=Owner, doc="Sets interval for display of serverads", syntax=("[interval]",))
     def ads_interval(self, data, protocol):
         """Sets interval for display of serverads. Syntax: /ads_interval [duration in seconds]"""
         if len(data) == 0:
@@ -51,23 +52,23 @@ class ServerAds(SimpleCommandPlugin):
             yield from protocol.send_message(self.ads_interval.__doc__)
             return
 
-    @command("ads_reload", role=Owner, doc="Reloads configuration values")
+    @Command("ads_reload", role=Owner, doc="Reloads configuration values")
     def ads_reload(self, data, protocol):
         """Reloads configuration values. Syntax: /ads_reload"""
         yield from self.load_config()
         yield from protocol.send_message("ServerAds reloaded!")
 
 
-#=======================================================================================================================
-    @asyncio.coroutine
+#===============================================================================
     def load_config(self):
         try:
             self.serverads_list = self.config.config.serverads['serverads_list']
             self.serverads_prefix = self.config.config.serverads['serverads_prefix']
             self.interval = self.config.config.serverads['serverads_interval']
+            self.logger.info("Configuration loaded")
             print("[%s] Configuration loaded successfully" % self.name)
         except Exception as e:
-            #self.logger.info("Error occured! %s" % e)
+            self.logger.warning("Configuration failed to load! Error: %s" % e)
             print("[%s] Error occured! %s" % (self.name, "Failed to load config values!"))
             self.serverads_list = ["Welcome to the server!", "Have a nice stay!"]
             self.serverads_prefix = "[SA]"
